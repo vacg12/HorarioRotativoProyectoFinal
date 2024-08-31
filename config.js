d21913a549c2aff5cbe425f14b09a83ef77a067d
vacg12 <valentinacg2910@gmail.com>
2024-08-31 00:11:37 -0400

    arreglo

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // archivo de configuraciones
 const PAGE_URL = process.env.NODE_ENV === 'production'
-    ? 'https://horariorotativoproyectofinal.onrender.com/'
+    ? 'https://horariorotativoproyectofinal.onrender.com'
     : 'http://localhost:3003';
 
     const MONGO_URL = process.env.NODE_ENV === 'production'
